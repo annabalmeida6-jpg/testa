1882434d3adb71be7e1bbf77b06c8dacf9311d4e
annabalmeida6-jpg <anna.b.almeida6@aluno.senai.br>
2025-09-06 03:47:29 +1400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html> 
+<html lang="pt-br"> 
+<head>
+<meta charset="UTF-8"> 
+<title>Formulário de Cadastro</title> 
+<link rel="stylesheet" href="style.css"> 
+</head>
+<body>
+<div class="container"> 
+<h2>LOGIN</h2> 
+<form action="cliente/cadastroc.php" method="get" class="button-form">
+<input type="submit" value="Cadastrar cliente">
+<form action="pedido/Pedido.php" method="get" class="button-form">
+<input type="submit" value="Fazer pedido">
+</form>
+</body>
+</html>
